756268bb5984570771054d101e58611fc82d33b0
Xeronith <xeronith@yahoo.com>
2016-06-09 09:11:21 +0430

    Add project files.

---
+++ AspNetMvcSqlCe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvcSqlCe.MvcApplication" Language="C#" %>
---
+++ AspNetMvcSqlCe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcSqlCe.Startup))]
+namespace AspNetMvcSqlCe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
